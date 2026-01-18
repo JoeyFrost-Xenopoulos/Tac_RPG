4ec37ba53cab7d35ad28c813d1ca010d9a54a7d1
Joey Frost-Xenopoulos <joeyfrostxenopoulos@trentu.ca>
2026-01-17 23:18:53 -0500

    Added health bar ui

--- modules/ui/bigbar.lua
+++ modules/ui/bigbar.lua
@@ -2,6 +2,8 @@
 local BigBar = {}
 
 local BigBar_Base = love.graphics.newImage("assets/ui/bars/BigBar_Base.png")
+local BigBar_Fill = love.graphics.newImage("assets/ui/bars/BigBar_Fill.png")
+
 local barQuads = {}
 local bw, bh = BigBar_Base:getDimensions()
 
@@ -21,25 +23,33 @@ function BigBar.draw(x, y, width, anchor)
 
     local _, _, lw = barLeftQuad:getViewport()
     local _, _, mw = barMidQuad:getViewport()
-    local _, _, rw = barRightQuad:getViewport()
-
-    local barX = x + 110
 
     if anchor == "right" then
-        barX = x - 185
+        local barX = x - 185
         love.graphics.draw(BigBar_Base, barRightQuad, barX, barY)
         local midX = barX - width
         local scaleX = width / mw
         love.graphics.draw(BigBar_Base, barMidQuad, midX, barY, 0, scaleX, 1)
         local leftX = midX - lw
         love.graphics.draw(BigBar_Base, barLeftQuad, leftX, barY)
+
+        local totalWidth = width + 28
+        local fillX = leftX + totalWidth + 50
+        local fillScaleX = -totalWidth / BigBar_Fill:getWidth()
+        love.graphics.draw(BigBar_Fill, fillX, barY, 0, fillScaleX, 1)
     else
+        local barX = x + 110
         love.graphics.draw(BigBar_Base, barLeftQuad, barX, barY)
         local midX = barX + lw
         local scaleX = width / mw
         love.graphics.draw(BigBar_Base, barMidQuad, midX, barY, 0, scaleX, 1)
         local rightX = midX + mw * scaleX
         love.graphics.draw(BigBar_Base, barRightQuad, rightX, barY)
+
+        local totalWidth = width + 28
+        local fillX = barX + 50
+        local fillScaleX = totalWidth / BigBar_Fill:getWidth()
+        love.graphics.draw(BigBar_Fill, fillX, barY, 0, fillScaleX, 1)
     end
 end
 
