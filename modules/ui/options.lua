3fa79d786d17e3add34307578ce0706683a5184e
Joey Frost-Xenopoulos <joeyfrostxenopoulos@trentu.ca>
2026-01-31 18:19:46 -0500

    changed position of options menu

--- modules/ui/options.lua
+++ modules/ui/options.lua
@@ -6,23 +6,26 @@ Options.menuImage = nil
 Options.scaleX = 0.85
 Options.scaleY = 0.85
 
+local quadW = {left = 308, mid = 154, right = 204}
+local quadH = {top = 310, mid = 310, bot = 310}
+
 function Options.load()
     Options.menuImage = love.graphics.newImage("assets/ui/menu/options_menu.png")
     local imgW, imgH = Options.menuImage:getDimensions()
 
     Options.variants = {
         {
-            topLeft   = love.graphics.newQuad(0,   0,   308, 310, imgW, imgH),
-            topMid    = love.graphics.newQuad(462, 0,   154, 310, imgW, imgH),
-            topRight  = love.graphics.newQuad(768, 0,   204, 310, imgW, imgH),
+            topLeft   = love.graphics.newQuad(0,   0,   quadW.left,  quadH.top, imgW, imgH),
+            topMid    = love.graphics.newQuad(462, 0,   quadW.mid,   quadH.top, imgW, imgH),
+            topRight  = love.graphics.newQuad(768, 0,   quadW.right, quadH.top, imgW, imgH),
             
-            midLeft   = love.graphics.newQuad(0,   462, 308, 310, imgW, imgH),
-            midMid    = love.graphics.newQuad(462, 462, 154, 310, imgW, imgH),
-            midRight  = love.graphics.newQuad(768, 462, 204, 310, imgW, imgH),
+            midLeft   = love.graphics.newQuad(0,   462, quadW.left,  quadH.mid, imgW, imgH),
+            midMid    = love.graphics.newQuad(462, 462, quadW.mid,   quadH.mid, imgW, imgH),
+            midRight  = love.graphics.newQuad(768, 462, quadW.right, quadH.mid, imgW, imgH),
 
-            botLeft   = love.graphics.newQuad(0,   768, 308, 310, imgW, imgH),
-            botMid    = love.graphics.newQuad(462, 768, 154, 310, imgW, imgH),
-            botRight  = love.graphics.newQuad(768, 768, 204, 310, imgW, imgH)
+            botLeft   = love.graphics.newQuad(0,   768, quadW.left,  quadH.bot, imgW, imgH),
+            botMid    = love.graphics.newQuad(462, 768, quadW.mid,   quadH.bot, imgW, imgH),
+            botRight  = love.graphics.newQuad(768, 768, quadW.right, quadH.bot, imgW, imgH)
         }
     }
 end
@@ -60,40 +63,43 @@ function Options.draw()
     local screenH = love.graphics.getHeight()
 
     local vidW, vidH = Options.video:getDimensions()
-    if vidW == 0 or vidH == 0 then
-        love.graphics.draw(Options.video, 0, 0)
-        return
+    if vidW > 0 and vidH > 0 then
+        local sx = screenW / vidW
+        local sy = screenH / vidH
+        love.graphics.draw(Options.video, 0, 0, 0, sx, sy)
     end
 
-    local sx = screenW / vidW
-    local sy = screenH / vidH
-    love.graphics.draw(Options.video, 0, 0, 0, sx, sy)
-
     if Options.menuImage and Options.variants then
-        
         local v = Options.variants[1]
-        local imgW, imgH = Options.menuImage:getDimensions()        
-        local scaledW = imgW * Options.scaleX
-        local scaledH = imgH * Options.scaleY       
-        local x = (screenW - scaledW) / 2
-        local y = (screenH - scaledH) / 2
+
+        local totalW = (quadW.left + quadW.mid + quadW.right) * Options.scaleX
+        local totalH = (quadH.top + quadH.mid + quadH.bot) * Options.scaleY        
+        local offsetX = 45
+        local x = (screenW - totalW) / 2 - offsetX
+        local y = (screenH - totalH) / 2
+
+        local col2X = x + (quadW.left * Options.scaleX) - 2
+        local col3X = col2X + (quadW.mid * Options.scaleX) - 5
+        
+        local row2Y = y + (quadH.top * Options.scaleY) - 5
+        local row3Y = row2Y + (quadH.mid * Options.scaleY) - 135
 
         love.graphics.setColor(1, 1, 1, 1)
         
-        -- Top row)
-        love.graphics.draw(Options.menuImage, v.topLeft,  x, y, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.topMid,   x + 306 * Options.scaleX, y, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.topRight, x + 456 * Options.scaleX, y, 0, Options.scaleX, Options.scaleY)
+        -- Top row
+        love.graphics.draw(Options.menuImage, v.topLeft,  x,     y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.topMid,   col2X, y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.topRight, col3X, y, 0, Options.scaleX, Options.scaleY)
 
         -- Middle row
-        love.graphics.draw(Options.menuImage, v.midLeft,  x, y + 306 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.midMid,   x + 306 * Options.scaleX, y + 306 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.midRight, x + 456 * Options.scaleX, y + 306 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.midLeft,  x,     row2Y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.midMid,   col2X, row2Y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.midRight, col3X, row2Y, 0, Options.scaleX, Options.scaleY)
 
         -- Bottom row
-        love.graphics.draw(Options.menuImage, v.botLeft,  x, y + 456 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.botMid,   x + 306 * Options.scaleX, y + 456 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
-        love.graphics.draw(Options.menuImage, v.botRight, x + 456 * Options.scaleX, y + 456 * Options.scaleY, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.botLeft,  x,     row3Y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.botMid,   col2X, row3Y, 0, Options.scaleX, Options.scaleY)
+        love.graphics.draw(Options.menuImage, v.botRight, col3X, row3Y, 0, Options.scaleX, Options.scaleY)
     end
 end
 
